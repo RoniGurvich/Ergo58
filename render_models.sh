101c3da239198e6e752c43003e464f48d115cd64
roni <roni@pc>
2025-01-15 14:36:22 +0200

    refactor - renamed `case` to `src`

--- render_models.sh
+++ render_models.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls case/parts | awk -F '.' '{print $1}' | xargs -I {} openscad ./case/parts/{}.scad -o ./models/{}.stl
-ls case/full_assembly | awk -F '.' '{print $1}' | xargs -I {}  openscad -o ./doc/3d/{}.stl ./case/full_assembly/{}.scad
-ls case/full_assembly | awk -F '.' '{print $1}' | xargs -I {}  openscad -o ./doc/img/{}.png --viewall --autocenter --imgsize=1024,1024  --colorscheme=BeforeDawn ./case/full_assembly/{}.scad
+ls src/parts | awk -F '.' '{print $1}' | xargs -I {} openscad ./src/parts/{}.scad -o ./models/{}.stl
+ls src/full_assembly | awk -F '.' '{print $1}' | xargs -I {}  openscad -o ./doc/3d/{}.stl ./src/full_assembly/{}.scad
+ls src/full_assembly | awk -F '.' '{print $1}' | xargs -I {}  openscad -o ./doc/img/{}.png --viewall --autocenter --imgsize=1024,1024  --colorscheme=BeforeDawn ./src/full_assembly/{}.scad
